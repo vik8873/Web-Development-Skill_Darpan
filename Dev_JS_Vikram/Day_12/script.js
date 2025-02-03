1a7e50e0b29aa7dafae9aa83ece3d580fe91186f
vikram <vikramee88@gmail.com>
2025-02-03 21:04:46 +0530

    day 12

---
+++ Dev_JS_Vikram/Day_12/script.js
@@ -0,0 +1,19 @@
+let storeInput = [];
+let userInput = (prompt("Enter Your Number"));
+let mainInput = parseInt(userInput)
+
+while (userInput != "End") {
+    storeInput.push(mainInput)
+    userInput = (prompt("Enter Your Number"))
+    mainInput = parseInt(userInput)
+}
+ function Sum(a1) {
+    let result = 0
+    for (let index = 0; index < a1.length; index++) {
+        result = result+a1[index];
+        
+    }
+    return result
+    
+ }
+ alert(`The Sum of number is ${Sum(storeInput)}`)
